e70510a2cdf8c34d25a38e65dbf63930cd188ba2
Sarah F <sarahelayna@gmail.com>
2018-01-16 08:19:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {artist: "song title", artist2: "song title2"};
 function updatePlaylist (playlist, artistName, songTitle) {
 return Object.assign({}, playlist, { [artistName]: songTitle });
 }
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+}
