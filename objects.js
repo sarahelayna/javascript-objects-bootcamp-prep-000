a3a783264665cf173210631fbc8a0a9cdbb7269c
Sarah F <sarahelayna@gmail.com>
2018-01-16 08:20:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ return Object.assign({}, playlist, { [artistName]: songTitle });
 }
 
 function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName;
+  return delete playlist.artistName;
 }
