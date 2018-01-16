fa16cb04b78d4405b9fe5b841df7189994bf196a
Sarah F <sarahelayna@gmail.com>
2018-01-16 08:15:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist: "song title", artist2: "song title2"};
 
 function updatePlaylist (playlist, artistName, songTitle) {
-Object.assign({}, playlist, { artist3: "song title3" })
+Object.assign({}, playlist, { artistName: songTitle })
 return playlist;
 }
